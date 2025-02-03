dec87e7f4ed73379371cb7e9026d2d144a73e8b1
Imre Bartis <imbartis@gmail.com>
2025-02-03 02:53:28 +0200

    data caching & revalidation with RSC

--- src/app/api-time/page.tsx
+++ src/app/api-time/page.tsx
@@ -1,27 +1,32 @@
-import { revalidateTag } from "next/cache";
+import { revalidateTag } from 'next/cache'
+// import { revalidatePath } from 'next/cache'
 
-import RevalidateAPITimeButton from "./RevalidateAPITimeButton";
+import RevalidateAPITimeButton from './RevalidateAPITimeButton'
 
 export default async function APITime() {
-  const timeReq = await fetch("http://localhost:8080/time", {
+  const timeReq = await fetch('http://localhost:8080/time', {
     next: {
-      tags: ["api-time"],
-    },
-  });
-  const { time } = await timeReq.json();
+      tags: ['api-time'],
+      revalidate: 2
+    }
+    // cache: 'no-store'
+  })
+
+  const { time } = await timeReq.json()
 
   async function onRevalidate() {
-    "use server";
-    revalidateTag("api-time");
+    'use server'
+    revalidateTag('api-time')
+    // revalidatePath('/api-time')
   }
 
-  console.log(`Render /api-time ${new Date().toLocaleTimeString()}`);
+  console.log(`Render /api-time ${new Date().toLocaleTimeString()}`)
 
   return (
     <div>
-      <h1 className="text-2xl">Time From API</h1>
-      <p className="text-xl">{time}</p>
+      <h1 className='text-2xl'>Time From API</h1>
+      <p className='text-xl'>{time}</p>
       <RevalidateAPITimeButton onRevalidate={onRevalidate} />
     </div>
-  );
+  )
 }
