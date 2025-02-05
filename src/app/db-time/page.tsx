b9d08f29fe12120da65e08f29b5e8fde68d24df7
Imre Bartis <imbartis@gmail.com>
2025-02-05 04:33:06 +0200

    use cache-busting with tags

--- src/app/db-time/page.tsx
+++ src/app/db-time/page.tsx
@@ -1,26 +1,26 @@
-import { revalidatePath, revalidateTag } from "next/cache";
-import { getDBTime, getDBTimeReal } from "./db-time";
+import { revalidatePath, revalidateTag } from 'next/cache'
+import { getDBTime, getDBTimeReal } from './db-time'
 
-import RevalidateDBTimeButton from "./RevalidateDBTimeButton";
+import RevalidateDBTimeButton from './RevalidateDBTimeButton'
 
 // export const dynamic = "force-dynamic";
 
 export default async function DBTime() {
-  const { time } = await getDBTimeReal();
+  const { time } = await getDBTime()
 
-  console.log(`Render /db-time ${new Date().toLocaleTimeString()}`);
+  console.log(`Render /db-time ${new Date().toLocaleTimeString()}`)
 
   async function onRevalidate() {
-    "use server";
-    revalidatePath("/db-time");
-    // revalidateTag("db-time");
+    'use server'
+    // revalidatePath('/db-time')
+    revalidateTag('db-time')
   }
 
   return (
     <div>
-      <h1 className="text-2xl">Time From DB</h1>
-      <p className="text-xl">{time}</p>
+      <h1 className='text-2xl'>Time From DB</h1>
+      <p className='text-xl'>{time}</p>
       <RevalidateDBTimeButton onRevalidate={onRevalidate} />
     </div>
-  );
+  )
 }
